f1c4f2ccb03c0cd0437cb4b86ca9879064cf33bb
Heather Kirk <hskirk@gmail.com>
2019-06-10 23:42:51 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,20 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :release_year, numericality:(less_than_or_equal_to Time.current.year) !not_released
+  validates :release_year, numericality :true, if: :is_released, unless: :check_year 
   validates :released, inclusion: {in: [true, false]}
   validates :genre, presence: true 
   
-  def not_released
-    !self.released
+  
+  def check_year
+    if self.release_year > Date.time.year
+      false 
+    else 
+      true 
+    end 
   end 
+  
+  def is_released
+    !!(self.released == true)
+  end 
+  
 end 

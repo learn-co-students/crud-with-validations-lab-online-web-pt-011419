6139c648096624e016ea66b870817fb5fcb97c98
Heather Kirk <hskirk@gmail.com>
2019-06-11 00:29:03 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,9 +29,9 @@ class SongsController < ApplicationController
   
   def update
     @song = Song.find(params[:id])
+    @song.update(song_params)
     
      if @song.valid? 
-       @song.update(song_params)
        redirect_to song_path(@song)
      else 
        render :edit
@@ -41,7 +41,7 @@ class SongsController < ApplicationController
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
-    redirect_to song_path(@song)
+    redirect_to songs_url
   end 
   
   private 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,20 +1,22 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :release_year, numericality :true, if: :is_released, unless: :check_year 
   validates :released, inclusion: {in: [true, false]}
-  validates :genre, presence: true 
-  
-  
-  def check_year
-    if self.release_year > Date.time.year
-      false 
-    else 
-      true 
-    end 
-  end 
-  
-  def is_released
+  validates :release_year, length: {is: 4},  if: :is_released?, unless: :check_year
+  validates :release_year, numericality: true, if: :is_released?, unless: :check_year
+  validates :title, uniqueness: {scope: [:release_year, :artist_name], message: "Artist cannot produce the same song in the same year."}
+
+  def is_released?
     !!(self.released == true)
-  end 
-  
+  end
+
+  def check_year
+    if self.release_year != nil
+      if  self.release_year > Date.today.year
+        errors.add(:release_year, "Year cannot be in the future")
+        false
+      else
+        true
+      end
+    end
+  end
 end 

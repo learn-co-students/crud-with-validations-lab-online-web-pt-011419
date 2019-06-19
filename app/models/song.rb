122ba9d8b9c9f8ed815087042d88a2b8f1572601
Kelsey Low <kklowlow@gmail.com>
2019-06-19 23:33:01 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,8 @@ class Song < ApplicationRecord
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
   
-  with_options if: released? do |s| # not required, but if present, validates...
+  with_options if: :released do |s| # not required, but if present, validates...
     s.validates :release_year, presence: true 
     s.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
-  end
-  
-  def released?
-    released 
   end
 end

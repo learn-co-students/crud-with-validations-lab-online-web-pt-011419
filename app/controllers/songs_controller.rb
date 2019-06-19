e34a1c11b1c7a707da4823a8ff1c3db8f20e5e9d
Kelsey Low <kklowlow@gmail.com>
2019-06-19 23:40:43 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,50 +1,51 @@
 class SongsController < ApplicationController
-  before_action :set_song, only:[:show, :edit, :update, :destroy]
 
-    def index
-      @songs = Song.all
-    end
+  def index
+    @songs = Song.all
+  end
 
-    def show
-    end
+  def show
+    @song = Song.find(params[:id])
+  end
 
-    def edit
-    end
+  def new
+    @song = Song.new
+  end
 
-    def new
-      @song = Song.new
-    end
+  def create
+    @song = Song.new(song_params)
 
-    def create
-      @song = Song.new(song_params)
-      if @song.save
-        redirect_to song_path(@song)
-      else
-        render :new
-      end
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
     end
+  end
 
-    def update
-      if @song.update(song_params)
-        redirect_to song_path(@song)
-      else
-        render :edit
-      end
-    end
+  def edit
+    @song = Song.find(params[:id])
+  end
 
-    def destroy
-      @song.destroy
-      redirect_to songs_path
+  def update
+    @song = Song.find(params[:id])
+
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
     end
-end
+  end
+
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_url
+  end
 
 private
 
   def song_params
-    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+    params.require(:song).permit(:title, :release_year, :released, :genre, :artist_name)
   end
 
-  def set_song
-    @song = Song.find(params[:id])
-  end
 end
